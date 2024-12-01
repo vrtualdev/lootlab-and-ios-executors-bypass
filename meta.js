32d5a1624ae8f3cf6cf76ee620bd05cf8404dae8
.vrtual.dev <emmadsonabdon@gmail.com>
2024-12-01 14:58:16 -0500

    meta.js

---
+++ meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        IOS EXECUTORS BYPASSER 
+// @author      .vrtual.dev
+// @version     1.0.0
+// @description  https://discord.gg/G3FDkbvtMx Please Join The Server It Took Me A While To Make
+// @license     MIT
+// @run-at      document-end
+// @match        *://loot-link.com/s?*
+// @match        *://loot-links.com/s?*
+// @match        *://lootlink.org/s?*
+// @match        *://lootlinks.co/s?*
+// @match        *://lootdest.info/s?*
+// @match        *://lootdest.org/s?*
+// @match        *://lootdest.com/s?*
+// @match        *://links-loot.com/s?*
+// @match        *://linksloot.net/s?*
+// @grant       none
+// @namespace   https://github.com/1dontgiveaf/AppleWare-Bypasser/
+// @downloadURL https://raw.githubusercontent.com/1dontgiveaf/AppleWare-Bypasser/main/user.js
+// @updateURL   https://raw.githubusercontent.com/1dontgiveaf/AppleWare-Bypasser/main/meta.js
+// ==/UserScript==
